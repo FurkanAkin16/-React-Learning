fdc317c26d5986db11e853086474c4ad52c56c46
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-05-25 13:47:20 +0300

    Add files via upload

--- Monster- Rolodex/src/App.jsx
+++ Monster- Rolodex/src/App.jsx
@@ -1,36 +1,51 @@
 import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.compononet";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: "Yuhia", lastName: "Zhang" },
-      company: "ZTM",
+      monsters: [],
+      searchField: "",
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName} ,I work at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState({
-                name: { firstName: "Andrei", lastName: "Neaogie" },
-              });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        <h1 className="app-title">Monster Rolodex</h1>
+        <SearchBox
+          className="search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="Search Monsters"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
